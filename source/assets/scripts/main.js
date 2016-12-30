f2c7ccdb810247188bf7d305a236e35db7a357d0
Logan McAnsh <l.m@loganmcansh.com>
2016-12-30 12:33:02 -0500

    starts of a new UI, plus search :tada:
    hit API allotment for the day to remain free.... so back to it tomorrow then
    
    Signed-off-by: Logan McAnsh <l.m@loganmcansh.com>

--- source/assets/scripts/main.js
+++ source/assets/scripts/main.js
@@ -4,14 +4,19 @@ let latitude,
     longitude,
     key = `8d82c87ab61ad64f2381f998a385bd14`,
     url,
-    geocode;
+    reverseGeocode,
+    geocode,
+    zip,
+    conditionsIcon;
 
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(function(position) {
     window.longitude = position.coords.longitude;
     window.latitude = position.coords.latitude;
+    /* eslint-disable */
     console.log(`latitude: ${latitude}`);
     console.log(`longitude: ${longitude}`);
+    /* eslint-disable */
 
     $('.lat').text(window.latitude);
     $('.long').text(window.longitude);
@@ -19,17 +24,18 @@ if (navigator.geolocation) {
     window.url = `https://api.darksky.net/forecast/${key}/${latitude},${longitude}`;
     console.log(url);
 
-    window.geocode = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyAc2pRb6K0kKlYCmaGrkyPTpYqjHoseUm0`;
+    window.reverseGeocode = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyAc2pRb6K0kKlYCmaGrkyPTpYqjHoseUm0`;
 
     $.ajax({
       type: 'GET',
-      url: geocode,
+      url: reverseGeocode,
       success: function(location) {
         /* eslint-disable */
-        console.log('converted your location to address');
+        console.log('got location from latitude/longitude');
         /* eslint-disable */
 
-        $('.location').text(`Your current location is ${location.results[0].address_components[2].short_name}`);
+        $('.location').show().val(location.results[0].address_components[2].short_name);
+
       },
       error: function() {
         /* eslint-disable */
@@ -49,58 +55,20 @@ if (navigator.geolocation) {
         /* eslint-disable */
 
         let temp = Math.round(data.currently.temperature);
-        $('.temp').text(`It's currently ${temp} degrees outside.`);
+        $('.temp').show().text(`It's currently ${temp} degrees outside.`);
 
         let conditions = data.currently.summary;
-        $('.conditions').text(`It's ${conditions}.`);
+        $('.conditions').show().text(`It's ${conditions}.`);
 
-        let conditionsIcon = data.currently.icon;
-        if (conditionsIcon === 'partly-cloudy-day') {
-          $('.icon').addClass('partly-cloudy-day');
-        }
-        else if (conditionsIcon === 'partly-cloudy-night') {
-          $('.icon').addClass('partly-cloudy-night');
-        }
-        else if (conditionsIcon === 'clear-day') {
-          $('icon').addClass('clear-day');
-        }
-        else if (conditionsIcon === 'clear-night') {
-          $('icon').addClass('clear-night');
-        }
-        else if (conditionsIcon === 'rain') {
-          $('icon').addClass('rain');
-        }
-        else if (conditionsIcon === 'snow') {
-          $('icon').addClass('snow');
-        }
-        else if (conditionsIcon === 'sleet') {
-          $('icon').addClass('sleet');
-        }
-        else if (conditionsIcon === 'wind') {
-          $('icon').addClass('wind');
-        }
-        else if (conditionsIcon === 'fog') {
-          $('icon').addClass('fog');
-        }
-        else if (conditionsIcon === 'cloudy') {
-          $('icon').addClass('cloudy');
-        }
-        else if (conditionsIcon === 'hail') {
-          $('icon').addClass('hail');
-        }
-        else if (conditionsIcon === 'thunderstorm') {
-          $('icon').addClass('thunderstorm');
-        }
-        else if (conditionsIcon === 'torndao') {
-          $('icon').addClass('torndao');
-        }
-        else {
-          $('icon').addClass('dead');
-        }
+        $('.later').show().text(data.hourly.summary);
+
+        window.conditionsIcon = data.currently.icon;
+
+        $('icon').attr('class', 'icon').addClass(conditionsIcon);
 
         let wind = Math.round(data.currently.windSpeed);
 
-        $('.wind').text(`The wind is ${wind} mph right now`);
+        $('.wind').show().text(`The wind is ${wind} mph right now`);
 
       },
       error: function() {
@@ -111,4 +79,92 @@ if (navigator.geolocation) {
     });
 
   });
+}
+else {
+  $('#content-wrapper').text('geolocation not supported, please upgrade your browser');
+}
+
+$('input').keyup(function() {
+  zip = $(this).val();
+});
+
+
+function submitForm() {
+  $('form').submit(function(event) {
+    event.preventDefault();
+    zipToLocation(zip);
+    /* eslint-disable */
+    console.log(`converting zip code: ${zip} to lat, long`);
+    /* eslint-disable */
+  });
+}
+
+function zipToLocation() {
+  window.geocode = `https://maps.googleapis.com/maps/api/geocode/json?address=${zip}`
+  console.log(geocode);
+
+  $.ajax({
+    type: 'GET',
+    url: geocode,
+    success: function(location) {
+      window.latitude = location.results[0].geometry.location.lat;
+      window.longitude = location.results[0].geometry.location.lng;
+      let city = location.results[0].address_components[1].short_name;
+
+
+      $('.location').show().val(city);
+
+      window.url = `https://api.darksky.net/forecast/${key}/${latitude},${longitude}`;
+
+      /* eslint-disable */
+      console.log(`got ${city} from latitude/longitude`);
+      /* eslint-disable */
+
+    },
+    error: function() {
+      /* eslint-disable */
+      console.log('error during conversion');
+      /* eslint-disable */
+    }
+  });
+
+  $.ajax({
+    type: 'GET',
+    url: url,
+    dataType: 'jsonp',
+    success: function(data) {
+      $('#loading').fadeOut(300);
+      /* eslint-disable */
+      console.log('got your weather');
+      /* eslint-disable */
+
+      let temp = Math.round(data.currently.temperature);
+      $('.temp').show().text(`It's currently ${temp} degrees outside.`);
+
+      let conditions = data.currently.summary;
+      $('.conditions').show().text(`It's ${conditions}.`);
+
+      $('.later').show().text(data.hourly.summary);
+
+      window.conditionsIcon = data.currently.icon;
+
+      $('icon').attr('class', 'icon').delay(2).addClass(conditionsIcon);
+
+      let wind = Math.round(data.currently.windSpeed);
+
+      $('.wind').show().text(`The wind is ${wind} mph right now`);
+
+    },
+    error: function() {
+      /* eslint-disable */
+      console.log('Error gathering weather, please refresh and try again.');
+      /* eslint-disable */
+    }
+  });
+}
+
+document.onkeydown=function(){
+  if(window.event.keyCode=='13'){
+    submitForm()
+  }
 }
