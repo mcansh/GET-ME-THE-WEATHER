fbae6198a7e6e9e99166d20f8f952499d0361377
Logan McAnsh <l.m@loganmcansh.com>
2016-12-30 12:51:16 -0500

    wrap main sauce in function
    theoretically you could register as dark sky dev, get token, 'key = ' and run 'doEverything()'
    
    Signed-off-by: Logan McAnsh <l.m@loganmcansh.com>

--- source/assets/scripts/main.js
+++ source/assets/scripts/main.js
@@ -9,83 +9,89 @@ let latitude,
     zip,
     conditionsIcon;
 
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    window.longitude = position.coords.longitude;
-    window.latitude = position.coords.latitude;
-    /* eslint-disable */
-    console.log(`latitude: ${latitude}`);
-    console.log(`longitude: ${longitude}`);
-    /* eslint-disable */
+function doEverything() {
 
-    $('.lat').text(window.latitude);
-    $('.long').text(window.longitude);
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      window.longitude = position.coords.longitude;
+      window.latitude = position.coords.latitude;
+      /* eslint-disable */
+      console.log(`latitude: ${latitude}`);
+      console.log(`longitude: ${longitude}`);
+      /* eslint-disable */
 
-    window.url = `https://api.darksky.net/forecast/${key}/${latitude},${longitude}`;
-    console.log(url);
+      $('.lat').text(window.latitude);
+      $('.long').text(window.longitude);
 
-    window.reverseGeocode = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyAc2pRb6K0kKlYCmaGrkyPTpYqjHoseUm0`;
+      window.url = `https://api.darksky.net/forecast/${key}/${latitude},${longitude}`;
+      console.log(url);
 
-    $.ajax({
-      type: 'GET',
-      url: reverseGeocode,
-      success: function(location) {
-        /* eslint-disable */
-        console.log('got location from latitude/longitude');
-        /* eslint-disable */
+      window.reverseGeocode = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyAc2pRb6K0kKlYCmaGrkyPTpYqjHoseUm0`;
 
-        $('.location').show().val(location.results[0].address_components[2].short_name);
+      $.ajax({
+        type: 'GET',
+        url: reverseGeocode,
+        success: function(location) {
+          /* eslint-disable */
+          console.log('got location from latitude/longitude');
+          /* eslint-disable */
 
-      },
-      error: function() {
-        /* eslint-disable */
-        console.log('error during conversion');
-        /* eslint-disable */
-      }
+          $('.location').show().val(location.results[0].address_components[2].short_name);
+
+        },
+        error: function() {
+          /* eslint-disable */
+          console.log('error during conversion');
+          /* eslint-disable */
+        }
+      });
+
+      $.ajax({
+        type: 'GET',
+        url: url,
+        dataType: 'jsonp',
+        success: function(data) {
+          $('#loading').fadeOut(300);
+          /* eslint-disable */
+          console.log('got your weather');
+          /* eslint-disable */
+
+          let temp = Math.round(data.currently.temperature);
+
+          $('.temp').show().text(`It's currently ${temp} degrees outside.`);
+
+          let conditions = data.currently.summary;
+          $('.conditions').show().text(`It's ${conditions}.`);
+
+          $('.later').show().text(data.hourly.summary);
+
+          window.conditionsIcon = data.currently.icon;
+
+          $('icon').attr('class', 'icon').addClass(conditionsIcon);
+
+          let wind = Math.round(data.currently.windSpeed);
+
+          $('.wind').show().text(`The wind is ${wind} mph right now`);
+
+        },
+        error: function() {
+          /* eslint-disable */
+          console.log('Error gathering weather, please refresh and try again.');
+          console.log('or hit api limit, come back tomorrow');
+          /* eslint-disable */
+          $('#loading h1').text(`Sorry, hit api limit, come back tomorrow...`)
+        }
+      });
+
     });
-
-    $.ajax({
-      type: 'GET',
-      url: url,
-      dataType: 'jsonp',
-      success: function(data) {
-        $('#loading').fadeOut(300);
-        /* eslint-disable */
-        console.log('got your weather');
-        /* eslint-disable */
-
-        let temp = Math.round(data.currently.temperature);
-
-        $('.temp').show().text(`It's currently ${temp} degrees outside.`);
-
-        let conditions = data.currently.summary;
-        $('.conditions').show().text(`It's ${conditions}.`);
-
-        $('.later').show().text(data.hourly.summary);
-
-        window.conditionsIcon = data.currently.icon;
-
-        $('icon').attr('class', 'icon').addClass(conditionsIcon);
-
-        let wind = Math.round(data.currently.windSpeed);
-
-        $('.wind').show().text(`The wind is ${wind} mph right now`);
-
-      },
-      error: function() {
-        /* eslint-disable */
-        console.log('Error gathering weather, please refresh and try again.');
-        console.log('or hit api limit, come back tomorrow');
-        /* eslint-disable */
-        $('#loading h1').text(`Sorry, hit api limit, come back tomorrow...`)
-      }
-    });
-
-  });
+  }
+  else {
+    $('#content-wrapper').text('geolocation not supported, please upgrade your browser');
+  }
 }
-else {
-  $('#content-wrapper').text('geolocation not supported, please upgrade your browser');
-}
+
+doEverything();
+
 
 $('input').keyup(function() {
   zip = $(this).val();
