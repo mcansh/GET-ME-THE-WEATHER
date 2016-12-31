37c1370ded48ffbbaf57b254369d74bd2f2ceb83
Logan McAnsh <logan@mcan.sh>
2016-12-31 00:22:25 -0500

    forgot to ignore my tokens
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- source/assets/scripts/tokens.js
+++ source/assets/scripts/tokens.js
@@ -1,2 +1,2 @@
-let key = `8d82c87ab61ad64f2381f998a385bd14`;
-// let key = 'a6273abb10867abab0e52678a696798b';
+let key = `8779683f32f33c37e5ce2c582e92c827`;
+// let key = 'eedac8fdb16eccd1eeea4ad0db3d3648';
