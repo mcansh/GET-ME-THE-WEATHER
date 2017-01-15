8f3f1f19478c434b46a23495115195f0ce17437f
Logan McAnsh <logan@mcan.sh>
2017-01-15 16:10:37 -0500

    pull in weather with fetch api
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- source/assets/scripts/main.js
+++ source/assets/scripts/main.js
@@ -1,131 +1,17 @@
-// @codekit-prepend 'jquery-3.1.1.min'
 // @codekit-prepend 'tokens'
 
 let latitude,
     longitude,
-    url,
+    url = `https://crossorigin.me/https://api.darksky.net/forecast/${key}/42.637118199999996,-83.0359869`,
     reverseGeocode,
     geocode,
     search,
-    conditionsIcon;
+    conditionsIcon,
+    loadingText = document.querySelector('#loading h1'),
+    welcomeText = document.querySelector('#welcome h1');
 
-function coordsToLocation() {
-  $.ajax({
-    type: 'GET',
-    url: reverseGeocode,
-    success: function(location) {
-      /* eslint-disable */
-      console.log('got location from latitude/longitude');
-      /* eslint-disable */
-      $('.location').show().attr('placeholder', location.results[0].address_components[2].short_name);
+let weather = [];
 
-    },
-    error: function() {
-      /* eslint-disable */
-      console.log('error during conversion');
-      /* eslint-disable */
-    }
-  });
-}
-
-function getWeather() {
-  $.ajax({
-    type: 'GET',
-    url: url,
-    dataType: 'jsonp',
-    success: function(data) {
-      $('#loading').fadeOut(300);
-      /* eslint-disable */
-      console.log('got your weather');
-      /* eslint-disable */
-
-      let temp = Math.round(data.currently.temperature);
-
-      $('.temp').show().text(`It's currently ${temp} degrees outside.`);
-
-      let conditions = data.currently.summary;
-      $('.conditions').show().text(`It's ${conditions}.`);
-
-      $('.later').show().text(data.hourly.summary);
-
-      window.conditionsIcon = data.currently.icon;
-
-      $('.icon').attr('class','icon').addClass(conditionsIcon);
-
-      let wind = Math.round(data.currently.windSpeed);
-
-      $('.wind').show().text(`The wind is ${wind} mph right now`);
-
-    },
-    error: function() {
-      /* eslint-disable */
-      console.log('Error gathering weather, please refresh and try again.');
-      console.log('or hit api limit, come back tomorrow');
-      /* eslint-disable */
-      $('#loading h1').text(`Sorry, hit api limit, come back tomorrow...`)
-    }
-  });
-}
-
-function doEverything() {
-
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      window.longitude = position.coords.longitude;
-      window.latitude = position.coords.latitude;
-      /* eslint-disable */
-      console.log(`latitude: ${latitude}`);
-      console.log(`longitude: ${longitude}`);
-      /* eslint-disable */
-
-      $('.lat').text(window.latitude);
-      $('.long').text(window.longitude);
-
-      window.url = `https://api.darksky.net/forecast/${key}/${latitude},${longitude}`;
-      console.log(url);
-
-      window.reverseGeocode = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyAc2pRb6K0kKlYCmaGrkyPTpYqjHoseUm0`;
-
-      coordsToLocation();
-
-      getWeather();
-
-    },
-    function(error) {
-      if (error.code == error.PERMISSION_DENIED) {
-        $('#loading h1').text('I need your location')
-      }
-    });
-  }
-  else {
-    $('#loading h1').text('geolocation not supported, please upgrade your browser');
-  }
-}
-
-doEverything();
-
-
-$('input').keyup(function() {
-  search = $(this).val();
-});
-
-
-function submitForm() {
-  $('form').submit(function(event) {
-    event.preventDefault();
-    searchToLocation(search);
-    /* eslint-disable */
-    console.log(`converting search: ${search} to lat, long`);
-    /* eslint-disable */
-  });
-}
-
-function searchToLocation(search) {
-
-}
-
-document.onkeydown=function(){
-  if(window.event.keyCode=='13'){
-    submitForm();
-  }
-}
+fetch(url)
+  .then(blob => blob.json())
+  .then(data => weather.push(...data));
--- source/assets/scripts/tokens.js
+++ source/assets/scripts/tokens.js
@@ -1,2 +1,3 @@
-let key = `8779683f32f33c37e5ce2c582e92c827`;
-// let key = 'eedac8fdb16eccd1eeea4ad0db3d3648';
+// let key = `8779683f32f33c37e5ce2c582e92c827`; // production
+let key = 'eedac8fdb16eccd1eeea4ad0db3d3648'; // testing 1
+// let key = '45003c767866bebcb9a3730063e9add0'; // testing 2
